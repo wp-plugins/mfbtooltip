4ca2c7f960dac4e0ee541dcf2cbd86b3c050427c
madeforbrowser <madeforbrowser@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-02 10:24:07 +0000

    removed bug in  including css file
    git-svn-id: https://plugins.svn.wordpress.org/mfbtooltip/trunk@1191165 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mfbtooltip.php
+++ mfbtooltip.php
@@ -3,7 +3,7 @@
 Plugin Name: mfbTooltip
 Plugin URI: http://daily.madeforbrowser.com/
 Description: simple tooltip, add class mfbtooltip  and data-tooltip="tooltipcontent" to all links. to show tooltip at bottom of link use class mfbtooltiprev instead of mfbtooltip
-Version: 1.0 
+Version: 1.0
 Author: Bjoern Zschernack
 Author URI: http://daily.madeforbrowser.com/
 License:GPL2
@@ -15,12 +15,12 @@ mfbTooltip is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 any later version.
- 
+
 mfbTooltip is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
- 
+
 You should have received a copy of the GNU General Public License
 along with mfbTooltip. If not, see http://www.gnu.org/licenses/gpl.html.
 
@@ -29,7 +29,7 @@ along with mfbTooltip. If not, see http://www.gnu.org/licenses/gpl.html.
  * enqueue style
  */
 function tooltip_styles() {
-   wp_enqueue_style( 'mfbtooltip', plugins_url().'/mfbTooltip/mfbtooltip.css' );
+   wp_enqueue_style( 'mfbtooltip', plugins_url().'/mfbtooltip/mfbtooltip.css' );
 }
-add_action( 'wp_enqueue_scripts', 'tooltip_styles' ); 
+add_action( 'wp_enqueue_scripts', 'tooltip_styles' );
 ?>
